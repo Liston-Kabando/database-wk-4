0a62928f881be4564216e8ef7983322111e0510d
Liston-Kabando <listonmacharia9@gmail.com>
2025-09-15 02:03:17 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -8,4 +8,10 @@ LIMIT 5;
 SELECT CustomerName, country, avg(creditlimit) AS avgcreditlimit
 FROM customers
 GROUP BY customerName, country;
+-- question 3
+SELECT productcode, quantityOrdered, sum(quantityOrdered*priceEach) AS totalProductPrice
+FROM orderdetails
+GROUP BY productCode, QuantityOrdered;
+
+
 
